6b89d807780dd17ebf01a922e8eceb11d7bb7048
DrFlower <90166542+therealdrflower@users.noreply.github.com>
2024-04-02 20:38:46 -0500

    Update basic.cpp

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -3,8 +3,11 @@
 
 
 int main() {
+    //Inits the TCON object map and file
     tcon::config test;
+    //Loads the file "example.tcon"
     tcon::loadFile(test, "example.tcon");
+    //Reads from the name "world"
     std::cout << tcon::readValue(test, "world");
     return 0;
 }
