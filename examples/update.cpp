875bedb5430c55bba6261e818f92b905c47c91d8
DrFlower <90166542+therealdrflower@users.noreply.github.com>
2024-04-02 20:38:17 -0500

    Update update.cpp

--- examples/update.cpp
+++ examples/update.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 
-#include "config.h"
+#include <tcon.h>
 
 
 int main() {
+    //Init the TCON object map and file
     tcon::config test;
+    //Loads the file
     tcon::loadFile(test, "example.tcon");
+    //Reads from the name "world"
     std::cout << tcon::readValue(test, "world");
     std::cout << '\n';
+    //Changes the value of "world" to "HELLO WORLD TEST UPDATE"
     tcon::changeValue(test, "world","HELLO WORLD TEST UPDATE");
     std::cout << tcon::readValue(test, "world");
+    //Writes the changes to the TCON file
     tcon::updateFile(test);
     return 0;
 }
